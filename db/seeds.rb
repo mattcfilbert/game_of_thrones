629680bfbc6717d39789d9b0e39511146e04f555
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-02 13:59:23 -0400

    cool beans

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,7 @@
 House.destroy_all
 
 stark = House.create(name: 'Stark', img_url: 'http://vignette4.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142')
+lan = House.create(name: 'Lannister', img_url: 'https://vignette2.wikia.nocookie.net/gameofthrones/images/0/0b/House-Lannister-heraldry.jpg/revision/latest?cb=20140402110342')
 
 arya = stark.characters.create(name: 'Arya', img_url: 'https://media.vanityfair.com/photos/54170a772e32fc85609d1b2f/master/pass/maisie_GOT_close.png')
+jaime = lan.characters.create(name: 'Jaime', img_url: 'https://upload.wikimedia.org/wikipedia/en/b/b4/Jaime_Lannister-Nikolaj_Coster-Waldau.jpg')
